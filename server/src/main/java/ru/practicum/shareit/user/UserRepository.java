d93777fe02524fb23d077e8a652b8e8b0be21e9b
Андрей Варначёв <nuuvik@yandex.ru>
2023-09-19 17:09:44 +0300

    refactor: разобрать приложение на докер-контейнеры

---
+++ server/src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.Nullable;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("select u from User u where  lower(u.email) = lower(:text)")
+    List<User> findByEmail(@Nullable String text);
+
+    Boolean existsByEmail(@Nullable String text);
+}
